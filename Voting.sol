f50d0c9c04a5c0eda5ae36554094b7fbea4c6aeb
Michał Kowieski <kowieski.m98@gmail.com>
2020-05-07 14:47:31 +0200

    Create Voting smart contract

---
+++ Voting.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.6.4;
+
+contract Voting {
+
+    mapping (bytes32 => uint256) public votes;
+
+    bytes32[] public candidates;
+
+    constructor(bytes32[] memory candidates_) public {
+        candidates = candidates_;
+    }
+
+    function readVotes(bytes32 candidate) public returns (uint256) {
+        require(isCandidateValid(candidate));
+        return votes[candidate];
+    }
+
+    function giveVote(bytes32 candidate) public {
+        require(isCandidateValid(candidate));
+        votes[candidate] += 1;
+    }
+
+    function isCandidateValid(bytes32 candidate) view public returns (bool) {
+        for (uint i = 0; i < candidates.lenght; ++i) {
+            if (candidates[i] == candidate) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
